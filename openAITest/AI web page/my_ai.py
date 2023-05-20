a05cfab3337bd0b53819babb9a121bded5bf7084
Hoon-Jung <hoonjung06@gmail.com>
2023-05-19 20:25:16 -0400

    updates to chatbot: now displays tags 'User: ' and 'chatbot: ' when using

--- openAITest/AI web page/my_ai.py
+++ openAITest/AI web page/my_ai.py
@@ -18,20 +18,28 @@ def index():
 
 @app.route('/', methods=["POST"])
 def receive_input():
+    userinlist = ["User: "]
+    aiinlist = ["Chatbot: "]
     user_input = request.form["message"]
+    userinlist.append(user_input)
     previous_inputs = []
     if 'all_prev_inputs' in session:
         previous_inputs = session["all_prev_inputs"]
-    previous_inputs.append(user_input)
+    previous_inputs.append(userinlist)
     
     response = openai.ChatCompletion.create(
     model= "gpt-3.5-turbo",
     messages= [{"role": "user", "content": user_input}]
     )
     
-    previous_inputs.append(response.choices[0].message["content"])
+    aiinlist.append(response.choices[0].message["content"])
+    previous_inputs.append(aiinlist)
     session["all_prev_inputs"] = previous_inputs
+    print(previous_inputs)
     return render_template("index.html", result=previous_inputs)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+#[[0, "user message"][1, "ai message"]]
